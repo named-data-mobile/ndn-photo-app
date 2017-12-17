3695cb341b879c7b4a7abb8457a5a19b45f2ab61
Nick Gordon <nmgordon@memphis.edu>
2017-12-17 14:11:28 -0600

    Fixed write permissions. Wrote packetize test function.

--- app/src/main/java/memphis/myapplication/MainActivity.java
+++ app/src/main/java/memphis/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package memphis.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -56,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
     SegmentFetcher fetcher;
     List<String> filesStrings = new ArrayList<String>();
     List<Uri> filesList = new ArrayList<Uri>();
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     private boolean has_setup_security = false;
     public void setup_security() {
@@ -101,6 +110,15 @@ public class MainActivity extends AppCompatActivity {
         this.filesList = new ArrayList<Uri>();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
     /** Called when the user taps the Send button */
@@ -151,6 +169,7 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
             File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "testfile");
+            file.setWritable(true);
             FileOutputStream os = new FileOutputStream(file);
             os.write(file_bytes);
             os.close();
