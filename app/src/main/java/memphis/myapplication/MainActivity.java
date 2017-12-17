a5822916e545ba9efeb5d8736f76340c09266558
Nick Gordon <nmgordon@memphis.edu>
2017-12-16 18:23:51 -0600

    Added NDN support. Can now register prefixes to NFD.

--- app/src/main/java/memphis/myapplication/MainActivity.java
+++ app/src/main/java/memphis/myapplication/MainActivity.java
@@ -1,10 +1,28 @@
 package memphis.myapplication;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import net.named_data.jndn.Face;
+import net.named_data.jndn.Interest;
+import net.named_data.jndn.InterestFilter;
+import net.named_data.jndn.Name;
+import net.named_data.jndn.OnInterestCallback;
+import net.named_data.jndn.OnRegisterFailed;
+import net.named_data.jndn.encoding.EncodingException;
+import net.named_data.jndn.security.KeyChain;
+import net.named_data.jndn.security.SecurityException;
+import net.named_data.jndn.security.identity.IdentityManager;
+import net.named_data.jndn.security.identity.MemoryIdentityStorage;
+import net.named_data.jndn.security.identity.MemoryPrivateKeyStorage;
+import net.named_data.jndn.security.pib.PibImpl;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -12,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     /** Called when the user taps the Send button */
@@ -28,5 +48,57 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, FileSelectActivity.class);
         startActivity(intent);
     }
+
+    public void show_dialog(Name prefix, boolean didFail) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        builder.setTitle("Prefix Registration failed? " + didFail).setMessage(prefix.toString()).show();
+
+    }
+
+    public void register_with_NFD(View view) throws IOException, PibImpl.Error {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String msg = editText.getText().toString();
+        Face face = new Face();
+
+        Name name = new Name(msg);
+        try {
+            MemoryIdentityStorage identityStorage = new MemoryIdentityStorage();
+            MemoryPrivateKeyStorage privateKeyStorage = new MemoryPrivateKeyStorage();
+            IdentityManager identityManager = new IdentityManager(identityStorage, privateKeyStorage);
+            KeyChain keyChain = new KeyChain(identityManager);
+            keyChain.setFace(face);
+
+            // NOTE: This is based on apps-NDN-Whiteboard/helpers/Utils.buildTestKeyChain()...
+            Name testIdName = new Name("/test/identity");
+            Name defaultCertificateName;
+            try {
+                defaultCertificateName = keyChain.createIdentityAndCertificate(testIdName);
+                keyChain.getIdentityManager().setDefaultIdentity(testIdName);
+            } catch (SecurityException e2) {
+                defaultCertificateName = new Name("/bogus/certificate/name");
+            }
+            face.setCommandSigningInfo(keyChain, defaultCertificateName);
+            long prefixId = face.registerPrefix(name,
+                    new OnInterestCallback() {
+                        @Override
+                        public void onInterest(Name prefix, Interest interest, Face face, long interestFilterId, InterestFilter filter) {
+                            show_dialog(prefix, false);
+                        }
+                    },
+                    new OnRegisterFailed() {
+                        @Override
+                        public void onRegisterFailed(Name prefix) {
+                            show_dialog(prefix, true);
+                        }
+                    });
+            face.processEvents();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (EncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
