8fce24f582ddf552a92da2eae7807d97eea78f50
Damian Coomes <damiancoomes@gmail.com>
2018-08-08 11:33:25 -0500

    updates

---
+++ app/src/main/java/memphis/myapplication/services/NetworkService.java
@@ -0,0 +1,34 @@
+package memphis.myapplication.services;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * This will be needed when we implement PSync. Until then, we will just use the Network Thread in
+ * MainActivity.
+ */
+public class NetworkService extends Service {
+
+    @Override
+    public void onCreate() {
+        // anything special?
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
